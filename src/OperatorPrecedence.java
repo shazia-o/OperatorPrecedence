dec6689c0bb30d021813123a67e84e0d0fc0ce6c
shazia-o <99616560+shazia-o@users.noreply.github.com>
2024-08-25 19:58:54 +0200

    Initial commit

---
+++ src/OperatorPrecedence.java
@@ -0,0 +1,20 @@
+public class OperatorPrecedence {
+
+    public static void main(String[] args) {
+
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+        System.out.println("myValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder"); //if bool operator from isNoRemainder is not true, display this message
+        }
+
+    }
+
+}
